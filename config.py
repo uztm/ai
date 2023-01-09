772323a623b7750b624532726dc9c250f5cb0870
uztm <93718406+uztm@users.noreply.github.com>
2023-01-09 18:14:11 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 TOKEN='5911163528:AAEveMEcq_TVmkVY9UArlf39d87zZ7rr5cI'
 
 CHANNELS = [
-  ["Канал 1", "-1001826805926", "https://t.me/tmportfolio"],
   ["Канал 2", "-1001882732417", "https://t.me/tmBots_bek"],
 ]
 
