fb224ec4896d439cdcee80b7872cee813bc2a22f
uztm <93718406+uztm@users.noreply.github.com>
2023-01-09 18:37:48 +0500

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-TOKEN='5911163528:AAEveMEcq_TVmkVY9UArlf39d87zZ7rr5cI'
-
-CHANNELS = [
-  ["Канал 2", "-1001882732417", "https://t.me/tmBots_bek"],
-]
-
-NOT_SUB_MESSAGE = "Для доступа к боту, необходимо подписаться на канал!"
