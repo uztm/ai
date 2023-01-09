a99c9bcfe074e6aec4c9a344d94b093ed5a5a288
uztm <93718406+uztm@users.noreply.github.com>
2023-01-09 19:28:03 +0500

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ async def start(message: types.Message):
 async def get_weather(message: types.Message):
   if message.chat.type == 'private':
     if await check_sub_channels(CHANNELS, message.from_user.id):
-      print(message.text)
+      print(message)
       model = replicate.models.get("stability-ai/stable-diffusion")
       version = model.versions.get("27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478")
       for image2 in version.predict(prompt=message.text):
